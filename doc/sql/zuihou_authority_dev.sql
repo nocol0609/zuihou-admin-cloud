61dcbf32e04e5cae97bc2ec3fb785f94b7dfe33c
tangyh <244387066@qq.com>
2019-08-02 10:51:02 +0800

    sql

--- doc/sql/zuihou_authority_dev.sql
+++ doc/sql/zuihou_authority_dev.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 123.56.221.77
+ Source Server         : 127.0.0.1
  Source Server Type    : MySQL
- Source Server Version : 50723
- Source Host           : 123.56.221.77:3306
+ Source Server Version : 50722
+ Source Host           : 127.0.0.1:3306
  Source Schema         : zuihou_authority_dev
 
  Target Server Type    : MySQL
- Target Server Version : 50723
+ Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 28/07/2019 23:39:28
+ Date: 02/08/2019 10:50:48
 */
 
 SET NAMES utf8mb4;
@@ -107,11 +107,9 @@ CREATE TABLE `c_auth_resource` (
   `update_user` bigint(20) DEFAULT NULL COMMENT '更新人id',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `UN_CODE` (`code`) USING BTREE COMMENT '编码唯一',
+  UNIQUE KEY `UN_CODE` (`code`) COMMENT '编码唯一',
   UNIQUE KEY `UN_URI` (`resource_type`,`micro_service_id`,`http_method`,`uri`) USING BTREE COMMENT 'URI唯一'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源';
-
-
 
 -- ----------------------------
 -- Table structure for c_auth_role
@@ -133,13 +131,6 @@ CREATE TABLE `c_auth_role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色';
 
 -- ----------------------------
--- Records of c_auth_role
--- ----------------------------
-BEGIN;
-INSERT INTO `c_auth_role` VALUES (1, '超级管理员', 'SUPER_ADMIN', '系统内置超级管理员，勿删', b'1', b'1', 5, 1, '2019-06-06 13:53:56', 1, '2019-06-06 13:53:59');
-COMMIT;
-
--- ----------------------------
 -- Table structure for c_auth_role_authority
 -- ----------------------------
 DROP TABLE IF EXISTS `c_auth_role_authority`;
@@ -154,28 +145,16 @@ CREATE TABLE `c_auth_role_authority` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色的资源';
 
 -- ----------------------------
--- Records of c_auth_role_authority
--- ----------------------------
-BEGIN;
-INSERT INTO `c_auth_role_authority` VALUES (1, 1, 'MENU', 1, '2019-06-26 11:35:07', 0);
-INSERT INTO `c_auth_role_authority` VALUES (2, 2, 'MENU', 1, NULL, 0);
-INSERT INTO `c_auth_role_authority` VALUES (3, 3, 'MENU', 1, NULL, 0);
-INSERT INTO `c_auth_role_authority` VALUES (4, 4, 'MENU', 1, NULL, 0);
-INSERT INTO `c_auth_role_authority` VALUES (5, 5, 'MENU', 1, NULL, 0);
-INSERT INTO `c_auth_role_authority` VALUES (6, 1, 'RESOURCE', 1, NULL, 0);
-COMMIT;
-
--- ----------------------------
 -- Table structure for c_auth_role_org
 -- ----------------------------
 DROP TABLE IF EXISTS `c_auth_role_org`;
 CREATE TABLE `c_auth_role_org` (
   `id` bigint(20) NOT NULL COMMENT 'ID',
   `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID\n#c_auth_role',
-  `org_id` bigint(20) DEFAULT NULL COMMENT '组织ID\n#c_core_org',
+  `org_id` bigint(20) DEFAULT NULL COMMENT '部门ID\n#c_core_org',
   `create_time` datetime DEFAULT NULL,
   `create_user` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色组织关系';
 
 -- ----------------------------
@@ -207,13 +186,6 @@ CREATE TABLE `c_auth_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户';
 
 -- ----------------------------
--- Records of c_auth_user
--- ----------------------------
-BEGIN;
-INSERT INTO `c_auth_user` VALUES (1, 'zuihou', 'zuihou', 1, 1, '1', 'M', b'1', b'0', '1', '1', 29, '2019-06-13', 0, '2019-09-07 09:56:54', 'E10ADC3949BA59ABBE56E057F20F883E', 1, '2019-07-28 11:08:20', 1, '2019-06-04 18:29:55');
-COMMIT;
-
--- ----------------------------
 -- Table structure for c_auth_user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `c_auth_user_role`;
@@ -225,14 +197,6 @@ CREATE TABLE `c_auth_user_role` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='角色分配\r\n账号角色绑定';
-
--- ----------------------------
--- Records of c_auth_user_role
--- ----------------------------
-BEGIN;
-INSERT INTO `c_auth_user_role` VALUES (1, 1, 1, 1, NULL);
-INSERT INTO `c_auth_user_role` VALUES (2, 2, 1, 1, NULL);
-COMMIT;
 
 -- ----------------------------
 -- Table structure for c_common_area
@@ -255,7 +219,6 @@ CREATE TABLE `c_common_area` (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='地区表';
 
-
 -- ----------------------------
 -- Table structure for c_common_dictionary
 -- ----------------------------
@@ -274,15 +237,6 @@ CREATE TABLE `c_common_dictionary` (
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='字典目录';
-
--- ----------------------------
--- Records of c_common_dictionary
--- ----------------------------
-BEGIN;
-INSERT INTO `c_common_dictionary` VALUES (1, 'NATION', 1, '民族', '民族', b'1', b'0', 1, '2019-06-01 09:42:50', 1, '2019-06-01 09:42:54');
-INSERT INTO `c_common_dictionary` VALUES (2, 'POSITION_STATUS', -1, '在职状态', NULL, b'1', b'0', 1, '2019-06-04 11:37:15', 1, '2019-06-04 11:37:15');
-INSERT INTO `c_common_dictionary` VALUES (3, 'EDUCATION', -1, '学历', NULL, b'1', b'0', 1, '2019-06-04 11:33:52', 1, '2019-06-04 11:33:52');
-COMMIT;
 
 -- ----------------------------
 -- Table structure for c_common_dictionary_item
@@ -307,72 +261,6 @@ CREATE TABLE `c_common_dictionary_item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='字典项';
 
 -- ----------------------------
--- Records of c_common_dictionary_item
--- ----------------------------
-BEGIN;
-INSERT INTO `c_common_dictionary_item` VALUES (40, 3, 'EDUCATION', '', '本科', b'1', b'0', '', 1, 1, '2019-06-04 11:36:19', 1, '2019-06-04 11:36:19');
-INSERT INTO `c_common_dictionary_item` VALUES (41, 3, 'EDUCATION', '', '博士', b'1', b'0', '', 1, 1, '2019-06-04 11:36:29', 1, '2019-06-04 11:36:29');
-INSERT INTO `c_common_dictionary_item` VALUES (43, 1, 'NATION', '', '汉族', b'1', b'0', '汉族', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (44, 1, 'NATION', '', '壮族', b'1', b'0', '壮族', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (45, 1, 'NATION', '', '满族', b'1', b'0', '满族', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (46, 1, 'NATION', '', '回族', b'1', b'0', '回族', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (47, 1, 'NATION', '', '苗族', b'1', b'0', '苗族', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (48, 1, 'NATION', '', '维吾尔族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (49, 1, 'NATION', '', '土家族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (50, 1, 'NATION', '', '彝族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (51, 1, 'NATION', '', '蒙古族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (52, 1, 'NATION', '', '藏族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (53, 1, 'NATION', '', '布依族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (54, 1, 'NATION', '', '侗族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (55, 1, 'NATION', '', '瑶族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (56, 1, 'NATION', '', '朝鲜族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (57, 1, 'NATION', '', '白族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (58, 1, 'NATION', '', '哈尼族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (59, 1, 'NATION', '', '哈萨克族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (60, 1, 'NATION', '', '黎族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (61, 1, 'NATION', '', '傣族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (62, 1, 'NATION', '', '畲族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (63, 1, 'NATION', '', '傈僳族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (64, 1, 'NATION', '', '仡佬族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (65, 1, 'NATION', '', '东乡族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (66, 1, 'NATION', '', '高山族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (67, 1, 'NATION', '', '拉祜族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (68, 1, 'NATION', '', '水族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (69, 1, 'NATION', '', '佤族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (70, 1, 'NATION', '', '纳西族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (71, 1, 'NATION', '', '羌族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (72, 1, 'NATION', '', '土族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (73, 1, 'NATION', '', '仫佬族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (74, 1, 'NATION', '', '锡伯族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (75, 1, 'NATION', '', '柯尔克孜族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (76, 1, 'NATION', '', '达斡尔族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (77, 1, 'NATION', '', '景颇族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (78, 1, 'NATION', '', '毛南族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (79, 1, 'NATION', '', '撒拉族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (80, 1, 'NATION', '', '塔吉克族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (81, 1, 'NATION', '', '阿昌族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (82, 1, 'NATION', '', '普米族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (83, 1, 'NATION', '', '鄂温克族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (84, 1, 'NATION', '', '怒族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (85, 1, 'NATION', '', '京族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (86, 1, 'NATION', '', '基诺族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (87, 1, 'NATION', '', '德昂族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (88, 1, 'NATION', '', '保安族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (89, 1, 'NATION', '', '俄罗斯族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (90, 1, 'NATION', '', '裕固族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (91, 1, 'NATION', '', '乌兹别克族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (92, 1, 'NATION', '', '门巴族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (93, 1, 'NATION', '', '鄂伦春族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (94, 1, 'NATION', '', '独龙族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (95, 1, 'NATION', '', '塔塔尔族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (96, 1, 'NATION', '', '赫哲族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (97, 1, 'NATION', '', '珞巴族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (98, 1, 'NATION', '', '布朗族', b'1', b'0', '', 1, 1, '2018-03-15 20:11:01', 1, '2018-03-15 20:11:04');
-INSERT INTO `c_common_dictionary_item` VALUES (99, 2, 'POSITION_STATUS', '', '在职', b'1', b'0', '', 1, 1, '2019-06-04 11:38:16', 1, '2019-06-04 11:38:16');
-INSERT INTO `c_common_dictionary_item` VALUES (100, 2, 'POSITION_STATUS', '', '离职', b'1', b'0', '', 1, 1, '2019-06-04 11:38:50', 1, '2019-06-04 11:38:50');
-COMMIT;
-
--- ----------------------------
 -- Table structure for c_common_opt_log
 -- ----------------------------
 DROP TABLE IF EXISTS `c_common_opt_log`;
@@ -383,7 +271,7 @@ CREATE TABLE `c_common_opt_log` (
   `user_name` varchar(50) DEFAULT '' COMMENT '操作人',
   `description` varchar(255) DEFAULT '' COMMENT '操作描述',
   `class_path` varchar(255) DEFAULT '' COMMENT '类路径',
-  `action_method` varchar(50) DEFAULT '' COMMENT '请求类型',
+  `action_method` varchar(50) DEFAULT '' COMMENT '请求方法',
   `request_uri` varchar(50) DEFAULT '' COMMENT '请求地址',
   `http_method` varchar(10) DEFAULT 'GET' COMMENT '请求类型\n#HttpMethod{GET:GET请求;POST:POST请求;PUT:PUT请求;DELETE:DELETE请求;PATCH:PATCH请求;TRACE:TRACE请求;HEAD:HEAD请求;OPTIONS:OPTIONS请求;}',
   `params` text COMMENT '请求参数',
@@ -398,7 +286,7 @@ CREATE TABLE `c_common_opt_log` (
   `create_user` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   KEY `index_type` (`type`) USING BTREE COMMENT '日志类型'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统日志';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='系统日志';
 
 -- ----------------------------
 -- Table structure for c_core_org
@@ -417,20 +305,9 @@ CREATE TABLE `c_core_org` (
   `create_user` bigint(20) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `update_user` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
+  PRIMARY KEY (`id`),
   FULLTEXT KEY `FU_PATH` (`tree_path`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='组织';
-
--- ----------------------------
--- Records of c_core_org
--- ----------------------------
-BEGIN;
-INSERT INTO `c_core_org` VALUES (1, '最后集团股份有限公司', '最后集团', -1, ',', 1, b'1', '初始化数据', '2019-07-10 17:02:18', 1, '2019-07-10 17:02:16', 1);
-INSERT INTO `c_core_org` VALUES (603634847700746593, '123', '123', -1, ',', 1, b'1', '123', '2019-07-24 17:09:23', 1, '2019-07-24 17:09:23', 1);
-INSERT INTO `c_core_org` VALUES (603641417591423009, '123', '123', -1, ',', 1, b'1', '123', '2019-07-24 17:35:29', 1, '2019-07-24 17:35:29', 1);
-INSERT INTO `c_core_org` VALUES (603641685313847393, '节点名称', '节点简称', -1, ',', 1, b'1', '节点描述', '2019-07-24 17:36:33', 1, '2019-07-24 17:36:33', 1);
-INSERT INTO `c_core_org` VALUES (603970147639626145, '用户中心', '', -1, ',', 1, b'1', '用户中心目录', '2019-07-25 15:21:45', 1, '2019-07-25 15:21:45', 1);
-COMMIT;
 
 -- ----------------------------
 -- Table structure for c_core_station
@@ -447,15 +324,7 @@ CREATE TABLE `c_core_station` (
   `create_user` bigint(20) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `update_user` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='岗位';
-
--- ----------------------------
--- Records of c_core_station
--- ----------------------------
-BEGIN;
-INSERT INTO `c_core_station` VALUES (1, '总经理', 1, 1, b'1', '总经理', '2019-07-10 17:03:03', 1, '2019-07-10 17:03:06', 1);
-INSERT INTO `c_core_station` VALUES (603609686662447137, '', 1, 1, b'1', '123', '2019-07-24 15:29:24', 1, '2019-07-24 15:29:24', 1);
-COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
